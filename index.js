f70ab738b43bd83b5629db2ff116b81d8a515c65
Shuvalov Anton <anton_shuvalov@me.com>
2015-02-05 11:04:14 +0300

    rewrite by es5

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = template => locals => template.replace(/\:([A-Za-z0-9_]+)/g, (noop, key) => locals[key]);
+'use strict';
+
+var matched = /\:([A-Za-z0-9_]+)/g;
+
+module.exports = function (template) {
+  return function(locals) {
+    if (!locals) locals = {};
+    return template.replace(matched, function(noop, key) {
+      return locals[key] || noop;
+    });
+  };
+};
