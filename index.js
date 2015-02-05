67a98497f5e73e98a18607674ccbd95a228b5667
Shuvalov Anton <anton_shuvalov@me.com>
2015-02-05 13:57:53 +0300

    add debug

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 'use strict';
 
+// Dependencies
+var debug = require('debug')('tiny-template');
+
+// Settings
 var matched = /\:([A-Za-z0-9_]+)/g;
+
 
 module.exports = function (template) {
   return function(locals) {
     if (!locals) locals = {};
     return template.replace(matched, function(noop, key) {
+      debug('render template `%s` with locals %o', template, locals);
       return locals[key] || noop;
     });
   };
