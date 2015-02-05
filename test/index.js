6a7778bd46d65917b1d074cdebaa0acd02fbec8c
Shuvalov Anton <anton_shuvalov@me.com>
2015-02-05 13:54:47 +0300

    remove logs from trash

--- test/index.js
+++ test/index.js
@@ -10,14 +10,11 @@ it('should replace :key with given locals', function() {
   var locals = { host: 'world', user: 'hello' };
   var template = 'ssh :user@:host';
   var result = tpl(template)(locals);
-  console.log(result);
   result.should.be.equal('ssh hello@world');
 });
 
 it('should not replace unmatched keys', function() {
   var template = 'ssh :user@:host';
   var result = tpl(template)();
-  console.log(result);
-
   result.should.equal('ssh :user@:host');
 });
